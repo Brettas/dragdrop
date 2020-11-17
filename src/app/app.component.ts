8d98ed0c5ba5487935c47ae5058f3d7449c8345c
Brettas <vinicius.palmeira@al.infnet.edu>
2020-11-17 16:22:13 -0300

    Arrumando as as colunas  e conectando com a tabela

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,10 +6,10 @@ import { ColumnMode } from '@swimlane/ngx-datatable';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css',
-]
+  ]
 })
 export class AppComponent {
-  
+
   title = 'app';
   loadingIndicator = true;
   reorderable = true;
@@ -17,46 +17,40 @@ export class AppComponent {
   ColumnMode = ColumnMode;
 
   dadosTabela = [
-    { name: 'Austin', gender: 'Male', company: 'Swimlane', },
-    { name: 'Dany', gender: 'Male', company: 'KFC' },
-    { name: 'Molly', gender: 'Female', company: 'Burger King', }
+    { name: 'Pedro', gender: 'M', company: 'Apple', skills: 'Python', state: 'RJ' },
+    { name: 'Bruno', gender: 'M', company: 'KFC', skills: ' TS', state: 'SP' },
+    { name: 'Lorena', gender: 'F', company: 'Coca-Cola', skills: 'JS', state: 'BA' },
+    { name: 'Leandro', gender: 'M', company: 'Burger King', skills: 'C#', state: 'AM' },
+    { name: 'Amanda', gender: 'F', company: 'UX', skills: 'Java', state: 'CE' }
   ];
   columns = [{ name: 'Name' }, { name: 'Gender' }, { name: 'Company' }];
+  colunasLivres = [{ name: 'Skills' }, { name: 'State' }];
 
 
-  skillsColumns = [{ name: 'Skill' }];
-
-  skills = [
-  { skills: 'Python' },
-  { skills: 'JS' },
-  { skills: 'TS' },
-  ]
+  
 
 
-
-
-
-
-
-  drop(event: CdkDragDrop<{ name: string, gender: string, company: string, id: string }[]>) {
+  drop(event: CdkDragDrop<{ name: string, gender: string, company: string, state: string, skills: string }[]>) {
     if (event.previousContainer === event.container) {
-      console.log('No mesmo bloco', moveItemInArray);
-      moveItemInArray(event.container.data,
-        event.previousIndex,
-        event.currentIndex);
-      this.dadosTabela = [...this.dadosTabela]
-      this.columns = [...this.columns]
-    } else {
-
-      transferArrayItem(event.previousContainer.data,
+      moveItemInArray(
         event.container.data,
         event.previousIndex,
         event.currentIndex);
-      this.dadosTabela = [...this.dadosTabela]
-      this.columns = [...this.columns]
-      console.log('Em bloco diferente', transferArrayItem);
+      this.dadosTabela = [...this.dadosTabela];
+      this.columns = [...this.columns];
+      this.colunasLivres = [...this.colunasLivres];
+    } else {
 
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex);
+      this.dadosTabela = [...this.dadosTabela];
+      this.columns = [...this.columns];
+      this.colunasLivres = [...this.colunasLivres];
     }
   }
+  
 
 }
